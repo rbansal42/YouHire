9491b26a4d5856d638012a3a74ccb72f834408c2
Rahul Bansal <42.rahulbansal@gmail.com>
2023-10-18 15:37:58 +0530

    Import all containers to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import {
+	Header,
+	Hero,
+	Stats,
+	Partners,
+	Collabs,
+	Testimonials,
+	Footer,
+} from "./containers";
+
 export default function App() {
 	return (
-	  <div className="container">
-		<div>Header</div>
-		<div>Hero</div>
-		<div>Stats</div>
-		<div>Our Partners</div>
-		<div>Our Collabs</div>
-		<div>Testimonials</div>
-		<div>Footer</div>
-	  </div>
-	)
-  }
+		<div className="bg-background text-text">
+			<Header></Header>
+			<Hero></Hero>
+			<Stats></Stats>
+			<Partners></Partners>
+			<Collabs></Collabs>
+			<Testimonials></Testimonials>
+			<Footer></Footer>
+		</div>
+	);
+}
