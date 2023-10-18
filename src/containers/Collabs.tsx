01e2fe623fdb45ebcdae262dfb71bc736103452a
Rahul Bansal <42.rahulbansal@gmail.com>
2023-10-18 14:59:01 +0530

    Structure for home page

---
+++ src/containers/Collabs.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Collabs = () => {
+	return <div>Collabs</div>;
+};
+
+export default Collabs;
---
+++ src/containers/index.ts
@@ -0,0 +1,17 @@
+import Header from "./Header";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Partners from "./Partners";
+import Collabs from "./Collabs";
+import Testimonials from "./Testimonials";
+import Footer from "./Footer";
+
+export default {
+	Header,
+	Hero,
+	Stats,
+	Partners,
+	Collabs,
+	Testimonials,
+	Footer,
+};
