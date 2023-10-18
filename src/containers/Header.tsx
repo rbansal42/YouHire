498a6c672860d1bae66a3f81b8b8552a9e3171d0
Rahul Bansal <42.rahulbansal@gmail.com>
2023-10-18 15:38:07 +0530

    header Complete

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<>
+			<div className="flex flex-row justify-between py-3 border-b-[0.5px] border-secondary">
+				<div>
+					<img
+						src="src/assets/images/logo.png"
+						alt="YouHire Logo"
+						width={90}
+						// height={10}
+					/>
+				</div>
+				<div>
+					<span>Get Hired!</span>
+				</div>
+				<div>
+					<span>Time</span>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Header;
