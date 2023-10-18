cb42bbe648c8ffe4330c75b7fe32a66e0a543057
Rahul Bansal <42.rahulbansal@gmail.com>
2023-10-18 14:58:52 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+export default function App() {
+	return (
+	  <div className="container">
+		<div>Header</div>
+		<div>Hero</div>
+		<div>Stats</div>
+		<div>Our Partners</div>
+		<div>Our Collabs</div>
+		<div>Testimonials</div>
+		<div>Footer</div>
+	  </div>
+	)
+  }
