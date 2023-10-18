0cec5697c63ec6703a44c3274270a25fff5579f7
Rahul Bansal <42.rahulbansal@gmail.com>
2023-10-18 17:44:51 +0530

    Built hero section

--- src/containers/Hero.tsx
+++ src/containers/Hero.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Hero = () => {
-	return <div>Hero</div>;
+	return (
+		<>
+			<div className="flex flex-row bg-hero-pattern h-96 bg-cover bg-no-repeat justify-center align-middle items-center">
+				<div className=" flex flex-row justify-between w-[50%]">
+					<button className="bg-primary hover:bg-gray-100 text-background py-2 px-4 border border-accent rounded shadow font-Poppins">
+						Corporate Login
+					</button>
+					<button className="bg-primary hover:bg-gray-100 text-background py-2 px-4 border border-accent rounded shadow font-Poppins">
+						Institute Login
+					</button>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Hero;
