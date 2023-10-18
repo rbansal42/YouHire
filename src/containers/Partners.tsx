1b309fc2d6b9f67833d806d55491f27bb00f0592
Rahul Bansal <42.rahulbansal@gmail.com>
2023-10-18 18:23:21 +0530

    Partner section done

--- src/containers/Partners.tsx
+++ src/containers/Partners.tsx
@@ -1,7 +1,42 @@
 import React from "react";
 
 const Partners = () => {
-	return <div>Partners</div>;
+	return (
+		<>
+			<div className="flex flex-col align-middle items-center py-8">
+				<div className="font-Poppins font-semibold text-2xl">
+					Our College Partners
+				</div>
+				<div className="flex flex-row justify-around w-full px-32 py-4 gap-2 border-b-[0.5px] border-accent">
+					<img
+						src="/src/assets/images/college1.png"
+						alt="college partner 1"
+						width={100}
+						className="object-contain"
+					/>
+					<img
+						src="src/assets/images/college2.png"
+						alt="college partner 2"
+						width={100}
+						className="object-contain"
+					/>
+					<img
+						src="src/assets/images/college3.png"
+						alt="college partner 3"
+						// height={50}
+						width={100}
+						className="object-contain h-[75px]"
+					/>
+					<img
+						src="src/assets/images/college4.jpg"
+						alt="college partner 3"
+						width={100}
+						className="object-contain"
+					/>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Partners;
