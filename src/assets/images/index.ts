352f776d5a27f4902e3d5f3caf9223128e1aa625
Rahul Bansal <42.rahulbansal@gmail.com>
2023-10-18 23:37:21 +0530

    Changed directory of images from public to src/assets
    Added index.ts

---
+++ src/assets/images/index.ts
@@ -0,0 +1,23 @@
+import logo from "./logo.png";
+
+import college1 from "./college1.png";
+import college2 from "./college2.png";
+import college3 from "./college3.png";
+import college4 from "./college4.png";
+
+import industry1 from "./industry1.webp";
+import industry2 from "./industry2.png";
+import industry3 from "./industry3.webp";
+import industry4 from "./industry4.webp";
+
+export {
+	logo,
+	college1,
+	college2,
+	college3,
+	college4,
+	industry1,
+	industry2,
+	industry3,
+	industry4,
+};
