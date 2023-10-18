4717ec3fd66b293aecbeb63768367bea89bb6626
Rahul Bansal <42.rahulbansal@gmail.com>
2023-10-18 17:45:12 +0530

    built stats section

--- src/containers/Stats.tsx
+++ src/containers/Stats.tsx
@@ -1,7 +1,36 @@
 import React from "react";
 
 const Stats = () => {
-	return <div>Stats</div>;
+	return (
+		<>
+			<div className="flex flex-row justify-around py-8 border-b-[0.5px] border-accent">
+				<div className="flex flex-col justify-center align-middle items-center text-center">
+					<div className="font-Poppins font-normal text-text">
+						Company Partners
+					</div>
+					<div className="font-Poppins font-semibold text-2xl text-text">
+						999+
+					</div>
+				</div>
+				<div className="flex flex-col justify-center align-middle items-center text-center">
+					<div className="font-Poppins font-normal text-text">
+						Students Placed
+					</div>
+					<div className="font-Poppins font-semibold text-2xl text-text">
+						9999+
+					</div>
+				</div>
+				<div className="flex flex-col justify-center align-middle items-center text-center">
+					<div className="font-Poppins font-normal text-text">
+						College Partners
+					</div>
+					<div className="font-Poppins font-semibold text-2xl text-text">
+						99+
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Stats;
