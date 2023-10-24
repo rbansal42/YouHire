38a16de9c6bfb1b28ccb210f9211077edfa9fed1
Rahul Bansal <42.rahulbansal@gmail.com>
2023-10-24 19:32:49 +0530

    enabled routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import {
-	Header,
-	Hero,
-	Stats,
-	Partners,
-	Collabs,
-	Testimonials,
-	Footer,
-} from "./containers";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { DashboardStudent, Home, LoginPage, ListingStudent } from "./pages";
 
 export default function App() {
 	return (
-		<div className="bg-background text-text">
-			<Header></Header>
-			<Hero></Hero>
-			<Stats></Stats>
-			<Partners></Partners>
-			<Collabs></Collabs>
-			<Testimonials></Testimonials>
-			<Footer></Footer>
+		<div className="w-full bg-background text-text">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<LoginPage />} />
+					<Route path="/dashboard" element={<DashboardStudent />} />
+					<Route path="/listing" element={<ListingStudent />} />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
